bf4714de03b8d18579e94df60f54d567d87bd4ff
Aaruni Kaushik <aaruni@aaruni-GL553VD.localdomain>
2020-01-29 10:06:26 +0100

    Minor improvements to poweroff bot

--- poweroff.py
+++ poweroff.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 import json
 import requests
@@ -6,7 +6,7 @@ import os
 import time
 
 apiURL="https://api.telegram.org/bot"
-botID=""    #your secret bot ID here
+botID=""	#secret bot iD
 offset=0
 while (1):
 	try:
@@ -18,7 +18,16 @@ while (1):
 		updateID=data['result'][0]['update_id']
 		userID=data['result'][0]['message']['from']['id']
 		userCommand=data['result'][0]['message']['text']
-		if(userID==):    #the user you want to listen from
-			if(userCommand=='poweroff'):
-				os.system('sudo /usr/sbin/poweroff -f')
 		offset=updateID+1
+		if(userID==):		#insert telegram user ID
+			if(userCommand=='poweroff'):
+				try:
+					r=requests.get(apiURL+botID+"/getUpdates?offset="+str(offset))
+				except requests.exceptions.RequestException as e:
+					print(e)
+				os.system('sudo /sbin/poweroff -f')
+			else:
+				try:
+					r=requests.get(apiURL+botID+"/sendMessage?chat_id="+str(userID)+"&text="+userCommand)
+				except requests.exceptions.RequestException as e:
+					print(e)
